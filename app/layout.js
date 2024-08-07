e933d25ee6b4da62a7fe3a67a31dde74896e639e
CinnamonfirJ <111068472+CinnamonfirJ@users.noreply.github.com>
2024-08-07 06:20:28 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,6 @@ const rokkitt = Rokkitt({
 export const metadata = {
   title: "Farmly",
   description: "AI powered farm monitor",
-  openGraph: {
-    images: "/assets/thumbnail.png",
-  },
 };
 
 export default function RootLayout({ children }) {
@@ -30,8 +27,9 @@ export default function RootLayout({ children }) {
         <meta property='og:description' content='AI powered farm monitor' />
         <meta
           property='og:image'
-          content='https://opengraph.b-cdn.net/production/images/779effd3-be15-4561-afbf-1cce5a26fa26.png?token=tjaYAIBKihQ00axIpCgxM39Vg96zeIDzHrmWiRELypA&height=1200&width=1200&expires=33259007520'
+          content='/public/assets/thumbnail/thumbnail.png'
         />
+        {/* https://opengraph.b-cdn.net/production/images/779effd3-be15-4561-afbf-1cce5a26fa26.png?token=tjaYAIBKihQ00axIpCgxM39Vg96zeIDzHrmWiRELypA&height=1200&width=1200&expires=33259007520 */}
 
         <meta name='twitter:card' content='summary_large_image' />
         <meta property='twitter:domain' content='farmly-deploy.vercel.app' />
@@ -43,10 +41,13 @@ export default function RootLayout({ children }) {
         <meta name='twitter:description' content='AI powered farm monitor' />
         <meta
           name='twitter:image'
-          content='https://opengraph.b-cdn.net/production/images/779effd3-be15-4561-afbf-1cce5a26fa26.png?token=tjaYAIBKihQ00axIpCgxM39Vg96zeIDzHrmWiRELypA&height=1200&width=1200&expires=33259007520'
+          content='/public/assets/thumbnail/thumbnail.png'
         />
 
-        {/* <meta property='og:image' content='/assets/thumbnail.png' /> */}
+        <meta
+          property='og:image'
+          content='/public/assets/thumbnail/thumbnail.png'
+        />
       </head>
       <body className={twMerge(rokkitt.variable, " antialiased")}>
         {children}
